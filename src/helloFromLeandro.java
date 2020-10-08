66dfec54052529077af465d48d6e4b9374fff209
GlaucioOliveira <glaucio.dev@gmail.com>
2020-10-07 22:37:54 -0300

    changing the message of the hello world :)

--- src/helloFromLeandro.java
+++ src/helloFromLeandro.java
@@ -1,6 +1,6 @@
 public class helloFromLeandro {
 
     public static void main (String arg []){
-        System.out.println("Hello, my name is Leandro Oliveira");
+        System.out.println("Hello, my name is Leandro Oliveira\n Hi Leandro, mine is Gláucio.");
     }
 }
